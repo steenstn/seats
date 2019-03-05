2b536f6e7bc7a7254519a7bcde71f4d184948c0d
steenstn <alest849@gmail.com>
2019-03-05 15:27:45 +0100

    Print active persons in green

--- Presenter.cpp
+++ Presenter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Presenter.h"
+#include <Windows.h>
 class Presenter;
 using namespace std;
 void Presenter::printMenu(std::vector<MenuCommand*> choices) {
@@ -12,8 +13,12 @@ void Presenter::printMenu(std::vector<MenuCommand*> choices) {
 }
 
 void Presenter::printConsultantList(std::vector<Person> consultants) {
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     for(int i = 0; i < consultants.size(); i++) {
+        int characterColor = consultants[i].isActive() ? FOREGROUND_GREEN | FOREGROUND_INTENSITY : FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED;
+        SetConsoleTextAttribute(out, characterColor);
         cout << i << ": " << consultants[i].getName() << endl;
     }
+    SetConsoleTextAttribute(out, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
     cout << endl;
 }
